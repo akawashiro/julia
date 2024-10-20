9ef88dab49bfb9bf5b6290bc79704a3bad62f690
Akira Kawata <akirakawata1@gmail.com>
2024-10-20 12:03:26 +0900

    Add benchmark

--- akawashiro/lorenz.jl
+++ akawashiro/lorenz.jl
@@ -1,3 +1,5 @@
+using DifferentialEquations
+
 function lorenz(u, p, t)
     dx = 10.0 * (u[2] - u[1])
     dy = u[1] * (28.0 - u[3]) - u[2]
@@ -5,8 +7,7 @@ function lorenz(u, p, t)
     [dx, dy, dz]
 end
 
-using DifferentialEquations, BenchmarkTools
 u0 = [1.0; 0.0; 0.0]
-tspan = (0.0, 100.0)
+tspan = (0.0, 1000.0)
 prob = ODEProblem(lorenz, u0, tspan)
-@btime solve(prob, Tsit5());
+solve(prob, Tsit5());
