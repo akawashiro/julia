a6fd1752e3f926a08e9c51a365e2a1aba487bd2d
Akira Kawata <3770618+akawashiro@users.noreply.github.com>
2024-12-31 13:04:12 +0900

    Run on CPU 0

--- akawashiro/lorenz_profile.sh
+++ akawashiro/lorenz_profile.sh
@@ -12,11 +12,20 @@ else
     git clone https://github.com/brendangregg/FlameGraph ${FLAMEGRAPH_DIR}
 fi
 
+# Profile julia with JIT compile
 ENABLE_JITPROFILING=1 perf record -F 99 -a --call-graph dwarf -k 1 -o akawashiro/lorenz.profile -- time ./julia akawashiro/lorenz.jl
 perf inject --jit --input akawashiro/lorenz.profile --output akawashiro/lorenz.jit.profile
 perf script --input akawashiro/lorenz.jit.profile | ${FLAMEGRAPH_DIR}/stackcollapse-perf.pl > akawashiro/lorenz.perf-folded
 ${FLAMEGRAPH_DIR}/flamegraph.pl akawashiro/lorenz.perf-folded > akawashiro/lorenz.svg
 
-perf record -F 99 -a --call-graph dwarf -k 1 -o akawashiro/lorenz_compile_no.profile -- time ./julia --compile=no akawashiro/lorenz.jl
+# Profile julia without JIT compile
+perf record \
+    -F 99 \
+    --call-graph dwarf \
+    -k 1 \
+    --cpu 0 \
+    -o akawashiro/lorenz_compile_no.profile \
+    -- taskset -c 0 \
+        ./julia --compile=no akawashiro/lorenz.jl
 perf script --input akawashiro/lorenz_compile_no.profile | ${FLAMEGRAPH_DIR}/stackcollapse-perf.pl > akawashiro/lorenz_compile_no.perf-folded
 ${FLAMEGRAPH_DIR}/flamegraph.pl akawashiro/lorenz_compile_no.perf-folded > akawashiro/lorenz_compile_no.svg
