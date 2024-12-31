5bd6f0ba46862aade0a8d4ae17b0e2d7d5f8e157
Akira Kawata <3770618+akawashiro@users.noreply.github.com>
2024-12-31 10:31:06 +0900

    Fix shellscript

--- akawashiro/nbody_profile.sh
+++ akawashiro/nbody_profile.sh
@@ -2,20 +2,21 @@
 
 set -eux -o pipefail
 root_dir=$(git rev-parse --show-toplevel)
+FLAMEGRAPH_DIR=/tmp/FlameGraph
 
 cd ${root_dir}
 
 if [ -d akawashiro/FlameGraph ]; then
     echo "FlameGraph already exists"
 else
-    git clone https://github.com/brendangregg/FlameGraph akawashiro/FlameGraph
+    git clone https://github.com/brendangregg/FlameGraph ${FLAMEGRAPH_DIR}
 fi
 
 ENABLE_JITPROFILING=1 perf record -F 99 -a --call-graph dwarf -k 1 -o akawashiro/nbody.profile -- time ./julia akawashiro/nbody.jl
 perf inject --jit --input akawashiro/nbody.profile --output akawashiro/nbody.jit.profile
-perf script --input akawashiro/nbody.jit.profile | akawashiro/FlameGraph/stackcollapse-perf.pl > akawashiro/nbody.perf-folded
-./akawashiro/FlameGraph/flamegraph.pl akawashiro/nbody.perf-folded > akawashiro/nbody.svg
+perf script --input akawashiro/nbody.jit.profile | ${FLAMEGRAPH_DIR}/stackcollapse-perf.pl > akawashiro/nbody.perf-folded
+${FLAMEGRAPH_DIR}/flamegraph.pl akawashiro/nbody.perf-folded > akawashiro/nbody.svg
 
 perf record -F 99 -a --call-graph dwarf -k 1 -o akawashiro/nbody_compile_no.profile -- time ${root_dir}/julia --compile=no --compiled-modules=no akawashiro/nbody.jl
 perf script --input akawashiro/nbody_compile_no.profile | akawashiro/FlameGraph/stackcollapse-perf.pl > akawashiro/nbody_compile_no.perf-folded
-./akawashiro/FlameGraph/flamegraph.pl akawashiro/nbody_compile_no.perf-folded > akawashiro/nbody_compile_no.svg
+${FLAMEGRAPH_DIR}/flamegraph.pl akawashiro/nbody_compile_no.perf-folded > akawashiro/nbody_compile_no.svg
