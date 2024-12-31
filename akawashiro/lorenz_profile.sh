7c9d74db9deda465ca238d0d5488abc56a235290
Akira Kawata <3770618+akawashiro@users.noreply.github.com>
2024-12-31 10:31:06 +0900

    Fix shellscript

--- akawashiro/lorenz_profile.sh
+++ akawashiro/lorenz_profile.sh
@@ -3,19 +3,20 @@
 set -eux -o pipefail
 
 root_dir=$(git rev-parse --show-toplevel)
+FLAMEGRAPH_DIR=/tmp/FlameGraph
 cd $root_dir
 
-if [ -d akawashiro/FlameGraph ]; then
+if [ -d ${FLAMEGRAPH_DIR} ]; then
     echo "FlameGraph already exists"
 else
-    git clone https://github.com/brendangregg/FlameGraph
+    git clone https://github.com/brendangregg/FlameGraph ${FLAMEGRAPH_DIR}
 fi
 
 ENABLE_JITPROFILING=1 perf record -F 99 -a --call-graph dwarf -k 1 -o akawashiro/lorenz.profile -- time ./julia akawashiro/lorenz.jl
 perf inject --jit --input akawashiro/lorenz.profile --output akawashiro/lorenz.jit.profile
-perf script --input akawashiro/lorenz.jit.profile | akawashiro/FlameGraph/stackcollapse-perf.pl > akawashiro/lorenz.perf-folded
-./akawashiro/FlameGraph/flamegraph.pl akawashiro/lorenz.perf-folded > akawashiro/lorenz.svg
+perf script --input akawashiro/lorenz.jit.profile | ${FLAMEGRAPH_DIR}/stackcollapse-perf.pl > akawashiro/lorenz.perf-folded
+${FLAMEGRAPH_DIR}/flamegraph.pl akawashiro/lorenz.perf-folded > akawashiro/lorenz.svg
 
 perf record -F 99 -a --call-graph dwarf -k 1 -o akawashiro/lorenz_compile_no.profile -- time ./julia --compile=no akawashiro/lorenz.jl
-perf script --input akawashiro/lorenz_compile_no.profile | akawashiro/FlameGraph/stackcollapse-perf.pl > akawashiro/lorenz_compile_no.perf-folded
-./akawashiro/FlameGraph/flamegraph.pl akawashiro/lorenz_compile_no.perf-folded > akawashiro/lorenz_compile_no.svg
+perf script --input akawashiro/lorenz_compile_no.profile | ${FLAMEGRAPH_DIR}/stackcollapse-perf.pl > akawashiro/lorenz_compile_no.perf-folded
+${FLAMEGRAPH_DIR}/flamegraph.pl akawashiro/lorenz_compile_no.perf-folded > akawashiro/lorenz_compile_no.svg
